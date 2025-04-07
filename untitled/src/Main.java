d2db709be0848620f00283ee4d069b5b569eb465
Antonio Paulo Barros <95260401+apabsp@users.noreply.github.com>
2025-04-07 18:22:31 -0300

    MySQL integrado ao código java

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,39 @@
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Teste!");
+        //con = DriverManager.getConnection("jdbc:mysql://localhost:3306/userdatenbank", "root", "adminadmin123");
+        String url = "jdbc:mysql://localhost:3306/biblioteca";
+        String user = "root";
+        String password = "12345";
+
+        try{
+            Connection conn = DriverManager.getConnection(url,user,password);
+            System.out.println("Connected!");
+            //This is just a test
+            String createTable = "CREATE TABLE IF NOT EXISTS obras (" +
+                    "id INT PRIMARY KEY AUTO_INCREMENT, " +
+                    "name VARCHAR(100) NOT NULL) ";
+
+            String insert = "INSERT INTO obras (name) VALUES (?)";
+            PreparedStatement insertStatement = conn.prepareStatement(insert);
+            insertStatement.setString(1,"Obra1");
+            insertStatement.executeUpdate();
+            System.out.println("Obra inserida");
+
+
+            // Read test
+            String selectSql = "SELECT * FROM obras";
+            ResultSet result = conn.createStatement().executeQuery(selectSql);
+            while( result.next() ){
+                System.out.println(result);
+                System.out.println("ID: " + result.getInt("id") + ", Name: " + result.getString("name"));
+            }
+
+            conn.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e); // optional catch
+        }
     }
 }
